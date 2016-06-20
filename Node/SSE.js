811ef5e3784ed035c89c93a0a7a2d76f400090a2
SanghviR <SanghviR@HYRDLT1360.ES.AD.ADP.com>
2016-06-21 01:27:11 +0530

    Node SSE

---
+++ Node/SSE.js
@@ -0,0 +1,54 @@
+var http = require('http');
+var sys = require('sys');
+var fs = require('fs');
+
+http.createServer(function(req, res) {
+  debugHeaders(req);
+
+  if (req.headers.accept && req.headers.accept == 'text/event-stream') {
+    if (req.url == '/events') {
+      console.log('constructing SSE ');
+      sendSSE(req, res);
+    } else {
+      res.writeHead(404);
+      res.end();
+    }
+  } else {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    //res.write(fs.readFileSync(__dirname + '/sse-node.html'));
+    res.write('Events page not found ');
+    res.end();
+  }
+}).listen(8000,'localhost');
+
+function sendSSE(req, res) {
+  res.writeHead(200, {
+    'Content-Type': 'text/event-stream',
+    'Cache-Control': 'no-cache',
+    'Connection': 'keep-alive',
+    'Access-Control-Allow-Origin' :'*',
+    'Access-Control-Allow-Methods': 'POST, GET, OPTIONS'
+  });
+
+  var id = (new Date()).toLocaleTimeString();
+
+  setInterval(function() {
+    constructSSE(res, id, (new Date()).toLocaleTimeString());
+  }, 5000);
+
+  constructSSE(res, id, (new Date()).toLocaleTimeString());
+  //res.end();
+}
+
+function constructSSE(res, id, data) {
+  res.write('id: ' + id + '\n');
+  res.write("data: " + data + '\n\n');
+}
+
+function debugHeaders(req) {
+  sys.puts('URL: ' + req.url);
+  for (var key in req.headers) {
+    sys.puts(key + ': ' + req.headers[key]);
+  }
+  sys.puts('\n\n');
+}
